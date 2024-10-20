010e890c0415e63c64e982fe2d6ea3e48d7a24ad
Doug Anderson444 <douganderson444@gmail.com>
2024-10-20 16:13:37 -0300

    [lipmaa]: add test data

--- src/lipmaa.rs
+++ src/lipmaa.rs
@@ -71,4 +71,97 @@ mod tests {
     fn lipmaa_four() {
         assert!(4.is_lipmaa());
     }
+
+    #[test]
+    fn test_array_tuple() {
+        // an array of tuples (index, * is_lipmaa (long hop), backlink index )
+        // Sequence number...	...backlinks to sequence number...
+        // 1	0
+        // 2	1
+        // 3	2
+        // 4	* 1
+        // 5	4
+        // 6	5
+        // 7	6
+        // 8	* 4
+        // 9	8
+        // 10	9
+        // 11	10
+        // 12	* 8
+        // 13	* 4
+        // 14	13
+        // 15	14
+        // 16	15
+        // 17	* 13
+        // 18	17
+        // 19	18
+        // 20	19
+        // 21	* 17
+        // 22	21
+        // 23	22
+        // 24	23
+        // 25	* 21
+        // 26	* 13
+        // 27	26
+        // 28	27
+        // 29	28
+        // 30	* 26
+        // 31	30
+        // 32	31
+        // 33	32
+        // 34	* 30
+        // 35	34
+        // 36	35
+        // 37	36
+        // 38	* 34
+        // 39	* 26
+        // 40	* 13
+        let lipmaabacklinks = [
+            (1, false, 0),
+            (2, false, 1),
+            (3, false, 2),
+            (4, true, 1),
+            (5, false, 4),
+            (6, false, 5),
+            (7, false, 6),
+            (8, true, 4),
+            (9, false, 8),
+            (10, false, 9),
+            (11, false, 10),
+            (12, true, 8),
+            (13, true, 4),
+            (14, false, 13),
+            (15, false, 14),
+            (16, false, 15),
+            (17, true, 13),
+            (18, false, 17),
+            (19, false, 18),
+            (20, false, 19),
+            (21, true, 17),
+            (22, false, 21),
+            (23, false, 22),
+            (24, false, 23),
+            (25, true, 21),
+            (26, true, 13),
+            (27, false, 26),
+            (28, false, 27),
+            (29, false, 28),
+            (30, true, 26),
+            (31, false, 30),
+            (32, false, 31),
+            (33, false, 32),
+            (34, true, 30),
+            (35, false, 34),
+            (36, false, 35),
+            (37, false, 36),
+            (38, true, 34),
+            (39, true, 26),
+            (40, true, 13),
+        ];
+
+        for (i, is_lipmaa_longhop, backlink) in lipmaabacklinks.iter() {
+            assert_eq!(i.lipmaa(), *backlink);
+            assert_eq!(i.is_lipmaa(), *is_lipmaa_longhop);
+        }
+    }
 }
